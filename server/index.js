2713b569b69b101bd9696526a8f1ba9c21f46ebd
Muhammad Jazlan Khan <142224933+MJazlanKhan@users.noreply.github.com>
2023-12-23 16:32:38 +0500

    Update

--- server/index.js
+++ server/index.js
@@ -4,20 +4,33 @@ import bodyParser from 'body-parser';
 import router from './routes/routes.js'
 import connectToMongo from './config/db.js';
 import { Server } from "socket.io";
+import path from "path"
 
+const Mode = "production"
 const app = express()
 app.use(bodyParser.json());
 const PORT = 9000;
 
 connectToMongo()
-app.get("/", (req, res) => {
-    res.send("API is Running....")
-});
+
 app.use(cors());
 app.use(express.json())
 app.use("/api/v1", router)
 
-const server = app.listen(PORT, () => console.log(`Server is Running on ${PORT}`))
+const __dirname1 = path.resolve();
+if(Mode === "production"){
+    app.use(express.static(path.join(__dirname1, "../client/build")))
+    app.get("*",(req, res)=>{
+        res.sendFile(path.resolve(__dirname1 , "client", "build", "index.html"))
+    })
+}else{
+    app.get("/", (req, res) => {
+    res.send("API is Running....")
+});
+}
+
+const server = app.listen(PORT, () => console.log(`Server is Running on ${PORT}`)
+)
 
 const io = new Server(server, {
     pingTimeout: 60000,
@@ -33,12 +46,12 @@ io.on("connection", (socket) => {
         console.log(userData)
         socket.emit('Connected')
     });
-    socket.on('join chat', (room)=>{
+    socket.on('join chat', (room) => {
         socket.join(room);
         console.log('user Joined Room ' + room)
     });
-    socket.on('typing',(room)=>socket.in(room).emit('typing'))
-    socket.on('stop typing',(room)=>socket.in(room).emit('stop typing'))
+    socket.on('typing', (room) => socket.in(room).emit('typing'))
+    socket.on('stop typing', (room) => socket.in(room).emit('stop typing'))
     socket.on('new message', (newMessageReceived) => {
         var chat = newMessageReceived.data;
         var users = chat.chat
@@ -47,18 +60,18 @@ io.on("connection", (socket) => {
         if (!users.users) {
             return console.log("Chat Users Not Defined");
         }
-    
+
         users.users.forEach((user) => {
-    
+
             if (user && user._id && user._id === chat.sender._id) {
                 return;
             }
-    
+
             // Emit the "message Received" event to the specific user
             socket.in(user._id).emit("messageReceived", chat);
             console.log(user);
         });
     });
-    
-    
+
+
 })
