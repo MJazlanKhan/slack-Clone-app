c6b59a25a1fbb1189c94b97fbb71188211e51848
Muhammad Jazlan Khan <142224933+MJazlanKhan@users.noreply.github.com>
2024-07-04 16:41:04 +0500

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 
-const URL = 'mongodb+srv://slackapp:Jazlan@cluster0.rvc4m0w.mongodb.net/?retryWrites=true&w=majority'
+const URL = 'mongodb+srv://slackapp:Jazlan123@cluster0.rvc4m0w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 const connectToMongo = async () => {
   try {
     const res = await mongoose.connect(URL);
