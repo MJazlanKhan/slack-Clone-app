35813207ee84961b40f1df9e82497b01e446c8a0
Muhammad Jazlan Khan <142224933+MJazlanKhan@users.noreply.github.com>
2024-07-04 16:49:10 +0500

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 
-const URL = 'mongodb+srv://slackapp:Jazlan123@cluster0.rvc4m0w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const ConnectDb = 'mongodb+srv://slackapp:Jazlan123@cluster0.rvc4m0w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 const connectToMongo = async () => {
   try {
-    const res = await mongoose.connect(URL);
+    const res = await mongoose.connect(ConnectDb);
     if(res){
         console.log("connection sucessfull")
     }
